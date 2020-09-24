3ca7f8da79aa80892cc59f963bc77b22d7d1216e
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-09-23 22:02:47 -0500

    setteando mapbox en angular

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
+  public map: mapboxgl.Map;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.createMap();
   }
 
+  public createMap() {
+    (mapboxgl as any).accessToken =
+      'pk.eyJ1IjoiZ2FicmllbDE0MDMiLCJhIjoiY2tmZzc3Ym5vMDJpNzJxbXp0NWx6ZTZieiJ9.LeR4T4_Y81Gdwaqcd4ixzw';
+    const map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+    });
+  }
 }
